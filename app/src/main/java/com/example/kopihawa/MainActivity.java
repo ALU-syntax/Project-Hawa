aa5f030c57bc7087e47cda74d4a2b965802523c2
woodiechek <ardianiqbal40@gmail.com>
2021-03-15 15:33:13 +0700

    Pindahan data dari local ke git
    Update List
    -Main Activity
    -Target Activity

---
+++ app/src/main/java/com/example/kopihawa/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kopihawa;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Target(View view) {
+        Intent intent = new Intent(MainActivity.this, TargetActivity.class);
+        startActivity(intent);
+    }
+}
+
